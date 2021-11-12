e414c967d5c7e46b938bfe205eb650f89e31d000
RizkyYusril <94151848+RizkyYusril@users.noreply.github.com>
2021-11-12 08:52:47 +0700

    Uploading my file

---
+++ script.js
@@ -0,0 +1,9 @@
+// var nama = prompt('Nama Kamu = ');
+// alert('Haloo' + " " + nama)
+
+var confirm = confirm('apakah kamu ilik ?');
+if(confirm === true) {
+    alert('howdy sir!');
+}else {
+    alert('fck off');
+}
